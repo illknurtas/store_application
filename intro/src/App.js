50510ece18ad4bae5f46a88efd83611b1236c7d0
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-20 18:26:10 +0300

    reactsrap and bootsrap installed

--- intro/src/App.js
+++ intro/src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import CategoryList from "./CategoryList";
 import Nav from "./Nav";
 import ProductList from "./ProductList";
+import {Container,Row, Col} from "reactstrap";
 
 function App() {
   return (
     <div>
-      <Nav/>
-      <CategoryList/>
-      <ProductList/>
+      <Container>
+        <Row>
+          <Nav/>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList/>
+          </Col>
+          <Col xs="9">
+            <ProductList/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
