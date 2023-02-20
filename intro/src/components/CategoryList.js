7c49de731308020142483fec889c25f5b684ccf5
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-20 19:17:46 +0300

    State update CategoryList.js

--- intro/src/components/CategoryList.js
+++ intro/src/components/CategoryList.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class CategoryList extends Component {
+  state={
+      categories:[
+        {
+          categoryId:1,
+          categoryName:"Beverages"
+        },
+        {
+          categoryId:2,
+          categoryName:"Condiments"
+        }
+      ]
+    };
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
-          <ListGroupItem>Item 1</ListGroupItem>
-          <ListGroupItem>Item 2</ListGroupItem>
-          <ListGroupItem>Item 3</ListGroupItem>
-          <ListGroupItem>Item 4</ListGroupItem>
-          <ListGroupItem>Item 5</ListGroupItem>
+          {
+            this.state.categories.map(
+              (category => (
+                <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+              )))
+          }
         </ListGroup>
       </div>
     )
