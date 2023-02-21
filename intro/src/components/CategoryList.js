93dc47abfa5dbfa1cf8b56cb1d5adddbef1ec813
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-21 18:18:09 +0300

    Update categories

--- intro/src/components/CategoryList.js
+++ intro/src/components/CategoryList.js
@@ -3,17 +3,16 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class CategoryList extends Component {
   state={
-      categories:[
-        {
-          categoryId:1,
-          categoryName:"Beverages"
-        },
-        {
-          categoryId:2,
-          categoryName:"Condiments"
-        }
-      ]
+      categories:[]
     };
+    componentDidMount(){
+      this.getCategories();
+    };
+    getCategories =()=>{
+      fetch("http://localhost:3000/categories")
+      .then(response => response.json())
+      .then(data => this.setState({categories:data}));
+    }
   render() {
     return (
       <div>
@@ -23,7 +22,7 @@ export default class CategoryList extends Component {
             this.state.categories.map(
               (category => (
                 <ListGroupItem onClick={()=> this.props.changeCategory(category)}
-                  key={category.categoryId}>
+                  key={category.id}>
                     {category.categoryName}
                   </ListGroupItem>
               )))
